08730540ba600be1427a6e1f01927ff56dd77f25
Sidorenkov Anton <asid.owner@gmail.com>
2023-07-07 21:03:35 +0400

    Autotest fix #1

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import hexlet.code.schemas.MapSchema;
+import hexlet.code.schemas.NumberSchema;
+import hexlet.code.schemas.StringSchema;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
